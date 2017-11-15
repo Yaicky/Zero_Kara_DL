ca65bb7d90f118a4cbf44cd735a3ad7213e95164
Yaicky <ajirencnty@gmail.com>
2017-11-15 22:54:53 +0900

    2017-11-15

---
+++ 1_Ronrikairo.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Yaicky'
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x*w) + b
+    return 0 if tmp<=0 else 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.3
+    tmp = np.sum(x*w) + b
+    return 0 if tmp<=0 else 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    return 0 if tmp<=0 else 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+print(XOR(0,0), XOR(0,1), XOR(1,0), XOR(1,1))
