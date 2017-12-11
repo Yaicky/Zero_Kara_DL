d103dfa6e31cb01e46f0f6327eeb169fd3d16bc4
Yaicky <ajirencnty@gmail.com>
2017-12-11 20:19:27 +0900

    2017-12-11

--- example/exercise01.py
+++ example/exercise01.py
@@ -1,16 +1,96 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Yaicky'
 import numpy as np
+from example.layers import *
+from collections import OrderedDict
 
-x = np.array([[2, 3, 4, 5], [2, 3, 4, 5]])
-t = np.array([1, 2])
-y = x.T
-#
-# print(x.shape)
-# print(x.ndim)
-# print(x.size)
-# print(y.shape)
-# print(y.ndim)
-# print(y.size)
-print(t.shape[0])
-print(x[np.arange(t.shape[0]), t])
+class TwoLayerNet():
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+        self.lastLayer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1: t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(t == y) / float(t.shape[0])
+        return accuracy
+
+    def gradient(self, x, t):
+        #forward
+        self.loss(x, t)
+
+        #backward
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
+
+
+if __name__ == '__main__':
+    from dataset.mnist import load_mnist
+
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+    network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+    iters_num = 10000
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.1
+
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+
+    iter_per_epoch = max(train_size / batch_size, 1)
+
+    for i in range(iters_num):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+
+        grad = network.gradient(x_batch, t_batch)
+
+        for key in ['W1', 'b1', 'W2', 'b2']:
+            network.params[key] -= grad[key] * learning_rate
+
+        loss = network.loss(x_batch, t_batch)
+        train_loss_list.append(loss)
+
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+            print(train_acc, test_acc)
