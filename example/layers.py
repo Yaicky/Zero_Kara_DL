1ec977f4897c05518a8cd89335c24ad635a6b5e5
Yaicky <ajirencnty@gmail.com>
2017-12-11 16:35:21 +0900

    2017-12-11

---
+++ example/layers.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Yaicky'
+import numpy as np
+
+class Affine():
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+
+        self.x = None
+        self.original_x_shape = None
+
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+
+        out = np.dot(self.x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        dx = dx.reshape(*self.original_x_shape)
+        return dx
+
+class SoftmaxWithLoss():
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def softmax(self, x):
+        # if x.ndim == 2:
+        #     x = x.T
+        #     x = x - np.max(x, axis=0)
+        #     y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        #     return y.T
+
+        x = x - np.max(x)
+        return np.exp(x) / np.sum(np.exp(x))
+
+    def cross_entropy_error(self, y, t):
+        if y.ndim == 1:
+            y = y.reshape(1, y.size)
+            t = t.reshape(1, t.size)
+
+        if y.size == t.size:
+            t = t.argmax(axis=1)
+
+        batch_size = y.shape[0]
+
+        return -np.sum(np.log(y[np.arange(batch_size), self.t])) / batch_size
+
+    def forward(self, x, t):
+        self.y = self.softmax(x)
+        self.t = t
+        self.loss = self.cross_entropy_error(self.y, self.t)
+
+        return  self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        if self.y.size == self.t.size:
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
+
+        return dx
+
+class Sigmoid():
+    def __init__(self):
+        self.out = None
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-1 * x))
+
+    def forward(self, x):
+        out = self.sigmoid(x)
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+class Relu():
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
