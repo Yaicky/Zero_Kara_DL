88797b202d7ca908e6189c484e434e7cb2063542
Yaicky <ajirencnty@gmail.com>
2017-12-12 22:57:56 +0900

    2017-12-12

--- example/test.py
+++ example/test.py
@@ -18,15 +18,18 @@ def softmax(x):
 x = np.array([[2, 3, 4, 5], [1, 2, 3, 4], [1, 4, 5, 6]])
 t = np.array([1, 2])
 y = x.T
-
-print(x.ndim)
-x1, x2 = x, x.T
-print(x1, x2)
-x1, x2 = x1 - np.max(x1), x2 - np.max(x2, axis=0)
-# rlt1 = x - np.max(x)
-# rlt2 =
-print(x1)
-print(x2)
+x3 = np.array([1, 2, 3, 4, 5
+            ])
+print(x3.ndim)
+print(np.max(x3))
+# print(x.ndim)
+# x1, x2 = x, x.T
+# print(x1, x2)
+# x1, x2 = x1 - np.max(x1), x2 - np.max(x2, axis=0)
+# # rlt1 = x - np.max(x)
+# # rlt2 =
+# print(x1)
+# print(x2)
 # [[ 0.0320586   0.08714432  0.23688282  0.64391426]]
 # [[ 0.0320586   0.08714432  0.23688282  0.64391426]]
 # print(x.shape)
